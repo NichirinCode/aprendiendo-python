5e19e2c9a1d8629a3d6b20cb7732d7dde5abbeea
NichirinCode <131426894+NichirinCode@users.noreply.github.com>
2025-08-16 01:44:43 +0200

    Add files via upload

---
+++ python-test-starts/01_suma.py
@@ -0,0 +1,6 @@
+#Un pequeño ejercicio para practicar la suma de dos números. 
+
+a = 1
+b = 2
+print("La suma de", a, "y", b, "es:", a + b)
+# Puedes cambiar los valores de a y b para probar con otros números.
