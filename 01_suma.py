f75de3e7733decd2b15cfe9aa349737e0b3f5b3c
NichirinCode <131426894+NichirinCode@users.noreply.github.com>
2025-08-16 01:43:58 +0200

    Delete 01_suma.py

--- 01_suma.py
+++
@@ -1,6 +0,0 @@
-#Un pequeño ejercicio para practicar la suma de dos números. 
-
-a = 1
-b = 2
-print("La suma de", a, "y", b, "es:", a + b)
-# Puedes cambiar los valores de a y b para probar con otros números.
