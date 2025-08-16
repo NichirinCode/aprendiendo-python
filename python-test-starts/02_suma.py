89b6ea2ac6fe8ff4c6a84929e78af63fb1d6588a
NichirinCode <131426894+NichirinCode@users.noreply.github.com>
2025-08-16 02:29:27 +0200

    Add files via upload

---
+++ python-test-starts/02_suma.py
@@ -0,0 +1,9 @@
+#Un ejercicio para que vosotros mismo podáis practicar y resolver por vuestra cuenta.
+#                     INTENTA HACERLO SIN MIRAR LA RESPUESTA.
+#Pedro fue a comprar al supermercado, compro 1 manzana, 2 peras y 3 naranjas. Cuantas frutas compro en total?
+# Respuesta:
+manzanas = 1
+peras = 2
+naranjas = 3    
+print("Pedro compro", manzanas + peras + naranjas, "frutas en total.")
+# Respuesta: Pedro compro 6 frutas en total
