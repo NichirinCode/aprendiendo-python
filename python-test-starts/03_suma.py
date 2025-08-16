5224483149251171420afcf1fd5e943ae002d235
NichirinCode <131426894+NichirinCode@users.noreply.github.com>
2025-08-16 02:36:44 +0200

    Add files via upload
    Recuerda, siempre intenta hacer los ejercicios por tu cuenta antes de mirar el resultado.

---
+++ python-test-starts/03_suma.py
@@ -0,0 +1,8 @@
+#Ahora vamos a por algo mas complicado, probemos a convertir un nombre y apellido a juntarlos como una sola palabra,
+#con la misma funcion que antes, pero ahora con un nombre y un apellido.
+nombre = "Juan"
+apellido = "Perez"
+print("El nombre completo es: " + nombre + apellido)
+#Si queremos que haya un espacio entre el nombre y el apellido, podemos hacerlo asi:
+print("El nombre completo es: " + nombre + " " + apellido)
+#AHORA PRUBARLO CON TU NOMBRE Y APELLIDO O IR VARIANDO LOS NOMBRES Y APELLIDOS.
