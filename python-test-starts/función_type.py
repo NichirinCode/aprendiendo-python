f511cdef6701801895c1a866c5db14712ddc9090
NichirinCode <131426894+NichirinCode@users.noreply.github.com>
2025-08-19 04:40:25 +0200

    Función Type
    Recuerda probar cambiar las variables a tu manera para ir practicando.

---
+++ python-test-starts/función_type.py
@@ -0,0 +1,11 @@
+#En los ejercicios anteriores hemos visto como se pueden modificar las variables y como se pueden cambiar sus tipos.
+#La funcion type() nos permite conocer el tipo de una variable en Python.
+#RECUERDA QUE PUEDES PROBAR HACER ESTOS EJERCICIOS A TU MANERA Y CON DIFERENTES VARIABLES.
+
+
+#A continuacion veremos como se puede utilizar la funcion type() para conocer el tipo de una variable.
+precioProducto = 150
+print(type(precioProducto))
+precioProducto = 250.50
+print(type(precioProducto))
+
