d0a6060433a2fe58279f8f2c4d38f51028cacce4
NichirinCode <131426894+NichirinCode@users.noreply.github.com>
2025-08-19 03:21:04 +0200

    Subamos un poco el nivel.
    Sacaremos el promedio, de un grupo de personas, utilizando los mismos pasos anteriores y agregando una pequeña división ( / ).

---
+++ python-test-starts/04_suma.py
@@ -0,0 +1,11 @@
+#Supongamos que necesitamos "calcular la edad promedio de los cuatro trabadajores de una empresa". Como lo haraiamos?
+# RECUERDA INTENTARLO POR TI MISMO ANTES DE MIRAR LA SOLUCION
+
+# Supongamos que tenemos la siguiente lista de edades de los trabajadores
+Marco = 30
+Ana = 25
+Pedro = 28
+Lucia = 32
+suma = Marco + Ana + Pedro + Lucia
+promedio = suma / 4
+print("La edad promedio de los trabajadores es:", promedio)
