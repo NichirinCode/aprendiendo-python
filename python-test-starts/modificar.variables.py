96dd13864b1719eb2cf58f0d351e55db4dcbfe76
NichirinCode <131426894+NichirinCode@users.noreply.github.com>
2025-08-19 04:25:02 +0200

    Modificaciones de variables.

---
+++ python-test-starts/modificar.variables.py
@@ -0,0 +1,21 @@
+#Las variables son un espacio de almacenamiento que se utiliza para guardar datos. 
+#En una variable se puede definir el precion de un producoty postriormente modificarlo.
+
+#En el siguiente codigo muestra como una variable puede ser modificada.
+
+precioPorducto = 150
+print("El precio del producto es:", precioPorducto)
+precioPorducto = 250
+print("El nuevo precio del producto es:", precioPorducto)
+
+#En este caso, la variable precioPorducto se inicializa con un valor de 150,
+#y luego se modifica a 250.
+
+#Ahora modificaremos el valor y el tipo de la variable.
+
+precioPorducto = 150
+print("El precio del producto es:", precioPorducto)
+precioPorducto = 250.50
+print("El nuevo precio del producto es:", precioPorducto)
+#En este caso, la variable precioPorducto se inicializa con un valor entero de 150,
+#y luego se modifica a un valor decimal de 250.50 tipo float.
