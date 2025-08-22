15d8efe630f73e79a9ac166474221726f2eea7b0
NichirinCode <131426894+NichirinCode@users.noreply.github.com>
2025-08-22 03:45:53 +0200

    Ahora haremos entrada y salida de datos.

---
+++ python-test-starts/Entrada y salida de datos..py
@@ -0,0 +1,17 @@
+#En este capitulo aprenderemos como ingresar daros en nuestros programas atraves del teclado (entrada).
+#Tambien aprenderemos a mostrar datos en la pantalla (salida).
+#Para esto usaremos las funciones input() y print().
+
+    #La funcion input() nos permite ingresar datos por teclado.
+    #La funcion print() nos permite mostrar datos en la pantalla.
+
+#Ejemplo de entrada y salida de datos:
+
+print("Hola, ¿Cual es tu nombre?")
+nombre = input() #Aqui se espera que el usuario ingrese su nombre
+print("Hola " + nombre + ", ¡Bienvenido a Python!") #Aqui se muestra un mensaje de bienvenida con el nombre ingresado
+
+#En este ejemplo, primero mostramos un mensaje pidiendo el nombre del usuario.bruno
+#Luego, usamos la funcion input() para capturar el nombre ingresado por el usuario y lo almacenamos en la variable 'nombre'.    
+#Finalmente, usamos la funcion print() para mostrar un mensaje de bienvenida que incluye el nombre del usuario.
+
